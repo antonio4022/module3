d07901454e588d5a6fe831e128159c58727b52ed
Antonio Alford-El <antonio.alford@gmail.com>
2021-04-16 21:51:33 -0400

    question 1 object practice submission

---
+++ objects/main.js
@@ -0,0 +1,16 @@
+/* write a javascrpt program to delete a property from a object
+1. define an object with properties that will used in the example.
+2. Use the delete key word on the object targeting the specific property
+
+*/
+
+
+var myStudent = {
+    name: "John",
+    age: 20,
+    address: "111 main street",
+    classes: 'javascript, react, html'
+
+};
+
+delete myStudent.classes;
