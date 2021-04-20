9bed3eae9c7d375cf616fb2cea5cd1280bbdc182
Antonio Alford-El <antonio.alford@gmail.com>
2021-04-20 18:40:43 -0400

    help with the last question

--- objects/question5.js
+++ objects/question5.js
@@ -2,16 +2,33 @@
 */
 
 
-let books = [
+let myBooks = [
     { author: 'Malcolm X', title: 'The Autobiography of Malcom X', LibraryID: 1247 },
     { author: 'Benot Lerone Jr.', title: 'Before the Mayflower', LibraryID: 1587 },
     { author: 'Cronin E.D.', title: 'Black Moses(The story of Garvey and the UNIA)', LibraryID: 56974 }
 ]
 
-books.sort(function (a, b) {
-    if (a.title.toLowerCase() < b.title.toLowerCase())
-        return -1
-    if (a.title.toLowerCase() > b.title.toLowerCase())
-        return 1;
-    return 0;
-})
+// arr.sort(function (a, b) {
+//     if (a.title.toLowerCase() < b.title.toLowerCase())
+//         return -1
+//     if (a.title.toLowerCase() > b.title.toLowerCase())
+//         return 1;
+//     return 0;
+
+// })
+
+function organizeBooks(arr, organizeBy) {
+    const bye = organizeBy.toLowerCase()
+    arr.sort(function (a, b) {
+
+        if (a.bye > b.bye)
+            return 1;
+
+        if (a.by < b.by)
+            return -1;
+        return 0;
+    })
+}
+let printMe = organizeBooks(myBooks, title)
+console.log(printMe);
+
